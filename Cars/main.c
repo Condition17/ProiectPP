4e9455f2bca395046fa6dd2336538b7deda43b97
miinth <stefan.vlad1996@yahoo.com>
2015-12-23 12:21:19 +0200

    modificare

--- Cars/main.c
+++ Cars/main.c
@@ -9,6 +9,7 @@ char afisare;
 struct player{
 int suma;
 int pozitie;
+int viteza;
 int hp;
 char nume[100]}player1,player2;
 void afisare_pista(){
@@ -65,20 +66,19 @@ pista2[80].obstacol=254;
 }
 void afisare_player1(){
     afisare_pista();
-printf("%s se afla pe pozitia %d la %d pozitii de urmatorul obstacol\n",player1.nume,player1.pozitie,distanta(player1.pozitie));
-printf("Pana la urmatoarea curba mai sunt %d pozitii",curba(player1.pozitie));
+printf("%s se afla pe pozitia %d la %d pozitii de urmatorul obstacol\n",player1.nume,player1.pozitie,distanta_obstacol(player1.pozitie));
+printf("Pana la urmatoarea curba mai sunt %d pozitii",distanta_curba(player1.pozitie));
 printf("\nMai aveti %d puncte de  viata",player1.hp);
-printf("\n\n\n\n\n\n\n\n\n");
+
 }
 void afisare_player2(){
     afisare_pista();
-printf("%s se afla pe pozitia %d la %d pozitii de urmatorul obstacol\n",player2.nume,player2.pozitie,distanta(player2.pozitie));
-printf("Pana la urmatoarea curba mai sunt %d pozitii",curba(player2.pozitie));
+printf("%s se afla pe pozitia %d la %d pozitii de urmatorul obstacol\n",player2.nume,player2.pozitie,distanta_obstacol(player2.pozitie));
+printf("Pana la urmatoarea curba mai sunt %d pozitii",distanta_curba(player2.pozitie));
 printf("\nMai aveti %d puncte de  viata",player2.hp);
-printf("\n\n\n\n\n\n\n\n\n");
 
 }
-int distanta(int pozitie){
+int distanta_obstacol(int pozitie){
 int ok=0, distanta=0; int initial=pozitie;
 while(!ok){pozitie=pozitie%101;
            if(pista1[pozitie].obstacol)ok=1;
@@ -87,18 +87,69 @@ while(!ok){pozitie=pozitie%101;
            }
         if(initial>pozitie)return distanta-2;
         return distanta-1;}
-int curba(int pozitie){
+int distanta_curba(int pozitie){
 if(pozitie<39)return 39-pozitie;
 if(pozitie<49)return 49-pozitie;
 if(pozitie<88)return 88-pozitie;
 if(pozitie<100)return 100-pozitie;}
-int main()
-{
+void viteza_player1(){int decizie;
+printf("\nSunteti in viteza %d.\nPentru a mari viteza cu o treapta apasati tasta 1.\nPentru a scadea viteza cu o treapta, apasati tasta 2.\nPebtru a scadea viteza cu doua trepte, apasati tasta 3.\nPentru a mentine viteza actuala, apasati tasta 4\n",player1.viteza);
+scanf("%d",&decizie);
+modificare_viteza(decizie);}
+void modificare_viteza(int decizie){
+int initial=player1.viteza;
+switch(decizie){
+case 1 :
+    player1.viteza++;
+    break;
+case 2 :
+    player1.viteza--;
+    break;
+case 3 :
+    player1.viteza-=2;
+    break;
+case 4 :
+    break;
+default :
+    limite_viteza();
+    break;}
+if(player1.viteza>5||player1.viteza<1){
+    player1.viteza=initial;
+    limite_viteza();
+}
+}
+void limite_viteza(){int decizie;
+printf("\nComanda nu se poate realiza, va rugam sa reintroduceti optiunea dorita\n");
+scanf("%d",&decizie);
+modificare_viteza(decizie);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(){
 initializare_obstacole();
 player1.hp=100;
 strcpy(player1.nume,"Stefan");
 player1.pozitie=95;
 pista1[player1.pozitie].afisare='@';
 afisare_player1();
+viteza_player1();
+
     return 0;
 }
